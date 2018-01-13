b30a33a27ced0b6b499a905cc01447eea1701462
Ritesh Sutaria <aadreja@gmail.com>
2018-01-14 00:24:11 +0530

    .

--- Vega/VegaTests/Common.cs
+++ Vega/VegaTests/Common.cs
@@ -4,7 +4,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Data.SQLite;
 using System.Linq;
 using System.Reflection;
 using Vega;
