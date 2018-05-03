71c2d0a20820e5d8354500e0f5bf90ffa1de7d16
Ritesh Sutaria <aadreja@gmail.com>
2018-05-03 16:00:35 +0530

    fixed null parameter errors in Read functions

--- Vega.Tests/InsertTests.cs
+++ Vega.Tests/InsertTests.cs
@@ -68,6 +68,7 @@ namespace Vega.Tests
             Repository<Country> countryRepo = new Repository<Country>(Fixture.Connection);
             var id = countryRepo.Add(country);
 
+            country.UpdatedBy = Fixture.CurrentUserId;
             var id1 = countryRepo.Update(country);
 
             Assert.Equal("India", countryRepo.ReadOne<string>(id, "Name"));
